975df8a19b02258e5304f616b0efdddf8192c614
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-07-09 20:10:49 -0500

    Clean up buttons and add shuffler

---
+++ assets/js/shuffle.js
@@ -0,0 +1,22 @@
+const getRandom = (min, max) => Math.floor(Math.random() * max + min);
+(async () => {
+  const shuffler = document.getElementById('shuffler');
+
+  if (!shuffler) {
+    return;
+  }
+
+  if (!window.contentApiKey) {
+    console.log('Error! No content API key was supplied. Shuffle posts button will not work');
+    return;
+  }
+
+  const {posts} = await fetch('/ghost/api/v3/content/posts/?limit=all&fields=url&key=' + contentApiKey)
+  .then(response => response.json());
+
+  debugger;
+  const post = posts[getRandom(0, posts.length)];
+
+  shuffler.href = post.url;
+  shuffler.classList.remove('disabled');
+})()
