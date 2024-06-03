7b6f690f8cdc42dad768d9be70acee547a109893
Vikas <potlurivikas@gmail.com>
2024-06-03 14:51:33 -0700

    fix(shuffler): exclude current post

--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -12,9 +12,12 @@ const getRandom = (min, max) => Math.floor(Math.random() * max + min);
   }
 
   const tag = new URLSearchParams(location.search).get('tag') || shuffler.dataset.tag;
-  const filter = tag || '-hash-newsletter';
   const href = tag ? '?tag=' + tag : '';
-  const {posts} = await fetch('/ghost/api/content/posts/?limit=all&fields=url&filter=tag:' + filter + '&key=' + window.contentApiKey)
+  const currentPost = shuffler.dataset.post;
+  let filter = 'tag:' + (tag || '-hash-newsletter');
+  filter = currentPost ? ('id:-' + currentPost + '+' + filter) : '';
+  filter = encodeURIComponent(filter)
+  const {posts} = await fetch('/ghost/api/content/posts/?limit=all&fields=url&filter=' + filter + '&key=' + window.contentApiKey)
   .then(response => response.json());
 
   const post = posts[getRandom(0, posts.length)];
