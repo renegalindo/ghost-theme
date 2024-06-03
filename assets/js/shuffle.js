949dbb937f84f6307eac79d9a4f54a06282be8d1
Vikas <potlurivikas@gmail.com>
2024-06-02 20:30:18 -0700

    es5 shuffler

--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -12,8 +12,8 @@ const getRandom = (min, max) => Math.floor(Math.random() * max + min);
   }
 
   const tag = shuffler.dataset.tag;
-  const filter = tag ? `tag:${tag}` : 'tag:-hash-newsletter';
-  const {posts} = await fetch(`/ghost/api/content/posts/?limit=all&fields=url&filter=${filter}&key=${contentApiKey}`)
+  const filter = tag || '-hash-newsletter';
+  const {posts} = await fetch('/ghost/api/content/posts/?limit=all&fields=url&filter=' + filter + '&key=' + window.contentApiKey)
   .then(response => response.json());
 
   const post = posts[getRandom(0, posts.length)];
